1c20812b3f79cf9109a515c0c467ab8d995825a8
Patrick Wolleb <patrick.wolleb@shmedia.co.uk>
2015-04-06 19:38:18 +0100

    Adds delete function to cart

--- public/js/app.js
+++ public/js/app.js
@@ -7,19 +7,7 @@ angle.module('wb')
 
 	add : function(item) {
 		var cart = this.get();
-		var i = cart.length;
-		var alreadyInCart = false;
-		while( --i > -1 ) {
-			if(cart[i]._id === item._id) {
-				alreadyInCart = true;
-				cart[i].count++;
-				break;
-			} 
-		}
-		if(!alreadyInCart) {
-			item.count = 1;
-			cart.push(item)
-		} 
+		cart.push(item);
 		this.persist(cart);
 	},
 
@@ -53,11 +41,9 @@ angle.module('wb')
 	selector : '.buy', 
 	link : function(element, Cart, Toast) {
 		element.addEventListener('click', function(e) {
-			
 			var product = JSON.parse(e.currentTarget.getAttribute('data-buy'));
 			Cart.add(product);
 			Toast.info(product.name + ' wurde dem Warenkorb hinzugefügt.')
-			//location.reload();
 		});
 	} 
 })
@@ -80,34 +66,73 @@ angle.module('wb')
 		var tFoot = element.querySelector('tfoot');
 		var total = 0;
 
+		function deleteItem(e) {
+			var index = e.currentTarget.getAttribute('data-index');
+			var tr = e.currentTarget.parentNode.parentNode;
+			cart.splice(index, 1);
+			Cart.persist(cart);
+			tr.parentNode.removeChild(tr);
+			total = 0;
+			cart.forEach(function(product) {
+				total += Math.round(product.content.price * 100) / 100;
+			})
+
+			// Calculate VAT and total
+			var vatAmount = Math.round(total / 100 * 8 * 100) / 100 ;
+			total = (Math.round((total + vatAmount) * 100) / 100).toFixed(2);
+		 	tFoot.querySelector('.price').innerHTML = 'SFr. ' +  (Math.ceil(total*20)/20).toFixed(2)
+			vat.querySelector('.price').innerHTML = 'SFr. ' +  (Math.round((vatAmount) * 100) / 100).toFixed(2);
+		
+			if(cart.length === 0) {
+				var p = document.createElement('p');
+				p.innerHTML = 'Ihr Warenkorb ist leer.'
+				p.setAttribute('class', 'col-xs-12')
+				element.querySelector('#empty').appendChild(p);
+				element.querySelector('#checkout-table').style.display = 'none';
+				element.querySelector('#checkout-form').style.display = 'none';
+			}
+
+		}	
+
+
+
+
 		// Render cart to table
-		cart.forEach(function(product){
+		cart.forEach(function(product, i){
 			var tr = document.createElement('tr');
 			var title = document.createElement('td');
 			title.innerHTML = product.name;
 			tr.appendChild(title);
-			var unitPrice = document.createElement('td');
-			unitPrice.innerHTML =  'SFr. ' + Math.round(product.content.price * 100) / 100;
-			tr.appendChild(unitPrice);
-			var quantity = document.createElement('td');
-			quantity.innerHTML =  product.count;
-			tr.appendChild(quantity);
 			var price = document.createElement('td');
-			price.innerHTML = 'SFr. ' + Math.round(product.content.price * product.count * 100) / 100;
+			price.innerHTML = 'SFr. ' + Math.round(product.content.price * 100) / 100;
 			price.setAttribute('class', 'price');
 			tr.appendChild(price);
-			total += Math.round(product.content.price * product.count * 100) / 100;
+			
+			var action = document.createElement('td');			
+			action.setAttribute('class', 'action');
+			tr.appendChild(action);
+
+			var deleteBtn = document.createElement('button');
+			deleteBtn.setAttribute('data-index', i);
+			deleteBtn.setAttribute('class', 'btn btn-xs btn-danger glyphicon glyphicon-remove')
+			deleteBtn.addEventListener('click', deleteItem)
+			action.appendChild(deleteBtn);
+
+
+			
+			total += Math.round(product.content.price * 100) / 100;
 			tBody.insertBefore(tr, vat);
 		});
 
 		// Calculate VAT and total
 		var vatAmount = Math.round(total / 100 * 8 * 100) / 100 ;
-	 	tFoot.querySelector('.price').innerHTML = 'SFr. ' +  (Math.round((total + vatAmount) * 100) / 100).toFixed(2);
-		vat.querySelector('.price').innerHTML = 'SFr. ' +  vatAmount;	
+	 	total = (Math.round((total + vatAmount) * 100) / 100).toFixed(2);
+		tFoot.querySelector('.price').innerHTML = 'SFr. ' +  (Math.ceil(total*20)/20).toFixed(2)
+		vat.querySelector('.price').innerHTML = 'SFr. ' +  (Math.round((vatAmount) * 100) / 100).toFixed(2);
 	
 		if(cart.length === 0) {
 			var p = document.createElement('p');
-			p.innerHTML = 'Ihr Warenkotb ist leer.'
+			p.innerHTML = 'Ihr Warenkorb ist leer.'
 			p.setAttribute('class', 'col-xs-12')
 			element.querySelector('#empty').appendChild(p);
 			element.querySelector('#checkout-table').style.display = 'none';
@@ -129,17 +154,17 @@ angle.module('wb')
 })
 
 
-.component({
-	selector : '#checkout-btn',
-	link : function(element, Cart) {
-		var cart = Cart.get();
-		if(cart.length !== 0) {
+// .component({
+// 	selector : '#checkout-btn',
+// 	link : function(element, Cart) {
+// 		var cart = Cart.get();
+// 		if(cart.length !== 0) {
 
-			var l = 0;
-			cart.forEach(function(product) {
-				l += product.count;
-			});
-			element.innerHTML = element.innerHTML + ' (' + l + ')'	
-		}
-	}
-})	
+// 			var l = 0;
+// 			cart.forEach(function(product) {
+// 				l += product.count;
+// 			});
+// 			element.innerHTML = element.innerHTML + ' (' + l + ')'	
+// 		}
+// 	}
+// })	
